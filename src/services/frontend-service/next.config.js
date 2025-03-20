951ca99c858688fc92c5c288c962017645ac507e
Jia Wei Ng <jay@base07.com>
2025-03-20 19:04:07 +0800

    feat: test pwa config

--- src/services/frontend-service/next.config.js
+++ src/services/frontend-service/next.config.js
@@ -29,6 +29,21 @@ const withPWAConfig = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+        },
+      },
+    },
+  ],
 });
 
 export default withPWAConfig(nextConfig);
